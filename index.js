e9274733982d94014aed91576f78e0ade692c7cc
Gordon Martin <gordon.hugh.martin@gmail.com>
2018-01-16 20:04:53 +0000

    Working on talk materials :P.

---
+++ index.js
@@ -0,0 +1,58 @@
+const ssbClient = require('ssb-client');
+const pull = require('pull-stream');
+
+function getUserLikesStream(sbot, userId) {
+  let userStream = sbot.createUserStream(userId);
+  return pull(userStream, pull.filter(isLikeMsg))
+}
+
+function isLikeMsg(msg) {
+  return msg.value && msg.value.content && msg.value.content.vote
+}
+
+function addToLikesCount(likesDict, author) {
+  if (likesDict[author]) {
+    likesDict[author] = likesDict + 1;
+  } else {
+    likesDict[author] = 1;
+  }
+}
+
+function likeMsgAuthorThrough(sbot) {
+  var likedMsgStream = pull.asyncMap((msg, cb) => sbot.get(msg.value.content.vote.link, cb));
+
+  return pull(likedMsgStream, pull.map(msg => {
+  //  console.log(msg.author);
+    return msg.author
+  }))
+}
+
+
+ssbClient((err, sbot) => {
+
+  if (err) {
+    console.log("aw naw");
+    console.log(err);
+    process.exit(1);
+  }
+
+  sbot.whoami((err, ident) => {
+    if (err) {
+      console.log("Aw naw, I don't know who I am.")
+      console.log(err);
+      process.exit(1);
+    }
+
+    var likesCount = {};
+
+    let userLikeStream = getUserLikesStream(sbot, ident);
+    let authorLikesStream = pull(userLikeStream, likeMsgAuthorThrough(sbot));
+
+    pull(authorLikesStream,
+       pull.reduce((count, author) => count + 1, 0, (err, result) => console.log("count: " + result)
+     ));
+
+  });
+
+
+});
